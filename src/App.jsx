85c6e51e9a099d7938edfba8cac98df4ec8aa2db
Cathy Chi <cathy@webigence.com>
2023-11-16 18:50:31 +0000

    Added album listing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/header'
+import AlbumListing from './components/albumlisting/albumlisting'
 
 function App() {
   const [count, setCount] = useState(0)
+  const topAlbums = [{
+      name: "101"
+    },
+    {
+      name: "202",
+    },
+    {
+      name: "303",
+    }
+  ]
+  const latestAlbums = [{
+      name: "101a"
+    },
+    {
+      name: "202b",
+    },
+    {
+      name: "303c",
+    }
+  ]
 
   return (
     <>
       <Header />
+      <AlbumListing heading="Top Albums" listing={topAlbums} />
+      <AlbumListing heading="Latest Albums" listing={latestAlbums} />
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [showProfileMenu, setShowProfileMenu] = useState(false);
+
+    function handleProfileButton() {
+        setShowProfileMenu(!showProfileMenu)
+    }
+
     return ( 
         <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
             <h1 className="text-2xl">Music Store</h1>
@@ -8,40 +16,43 @@ export default function Header() {
                 </a>
 
                 <div className="relative inline-block text-left">
-                    <button type="button" className="inline-flex items-center">
+                    <button type="button" className="inline-flex items-center" onClick={handleProfileButton}>
                         <span className="material-icons"> account_circle </span>
                         <span className="ml-2">Profile</span>
                         <span className="material-icons"> arrow_drop_down </span>
                     </button>
-                    <div
-                        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-                    >
+                    {
+                        showProfileMenu &&
                         <div
-                        className="py-1"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="options-menu"
+                            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
                         >
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            role="menuitem"
-                            >Your Profile</a
-                        >
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            role="menuitem"
-                            >Settings</a
-                        >
-                        <a
-                            href="#"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                            role="menuitem"
-                            >Sign out</a
-                        >
+                            <div
+                            className="py-1"
+                            role="menu"
+                            aria-orientation="vertical"
+                            aria-labelledby="options-menu"
+                            >
+                            <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                role="menuitem"
+                                >Your Profile</a
+                            >
+                            <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                role="menuitem"
+                                >Settings</a
+                            >
+                            <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                role="menuitem"
+                                >Sign out</a
+                            >
+                            </div>
                         </div>
-                    </div>
+                    }
                 </div>
             </div>
         </header>
